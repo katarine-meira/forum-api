4f72562029cb16f83b7b595831ad7056bf67b8d2
Katarine-meira <katarine.kmo@gmail.com>
2025-10-09 12:17:49 -0300

    feat:  create router user and post

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [AuthModule, UserModule], //modulo principal, todos importados aqui
-  controllers: [], //apenas controla
-  providers: [], //a lógica(regra de negocio)
+  imports: [AuthModule, UserModule, DatabaseModule], //modulo principal, todos importados aqui
+  // controllers: [], //apenas controla
+  // providers: [], //a lógica(regra de negocio)
 })
 export class AppModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+import { Prisma, User } from '@prisma/client';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  @Inject()
+  private readonly prisma: PrismaService;
+
+  //pega um usuário
+  async user(
+    userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+  ): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: userWhereUniqueInput,
+    });
+  }
+
+  //o prisma cria varíos tipos, de acordo com nosso bd
+  async createUser(data: Prisma.UserCreateInput) {
+    return this.prisma.user.create({ data });
+  }
+
+  async updateUser(params: {
+    where: Prisma.UserWhereUniqueInput; //where é o id
+    data: Prisma.UserUpdateInput; //tipo para update
+  }): Promise<User> {
+    const { where, data } = params;
+    return this.prisma.user.update({
+      data,
+      where,
+    });
+  }
+
+  async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+    return this.prisma.user.delete({
+      where,
+    });
+  }
+}
