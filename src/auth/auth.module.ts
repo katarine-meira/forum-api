5ef9d86eabb7201681427a9cef23f9597843a047
Katarine <katarine.kmo@gmail.com>
2025-10-12 23:46:46 -0300

    feat: token create

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, 
+    JwtModule.register({
+      global: true,
+      secret: process.env.SECRET_KEY || '', //passando o secret ao importar
+      signOptions: { expiresIn: '86400s' },
+    }),],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,28 +4,30 @@ import {
   NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
-import { Prisma, User } from '@prisma/client';
+import { Prisma } from '@prisma/client';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
-@Injectable()
+@Injectable() //torna a class AuthService como um provider injetavel em controladores (como o AuthCotroller) ou outros servicos
 export class AuthService {
   @Inject()
-  private readonly userService: UserService;
+  private readonly userService: UserService; // injeta o provider UserService pra poder ser reutilizado funções do prisma (como consulta de usuário)
+
+  @Inject()
+  private readonly jwtService: JwtService;
 
   //email e password
-  async singin(
-    params: Prisma.UserCreateInput,
-  ): Promise<Omit<User, 'password'>> {
+  async signin(
+    params: Prisma.UserCreateInput): Promise<{access_token: string}> {
     //retorna um tipo omit para omitir o password do user
-    const user = await this.userService.user({ email: params.email });
+    const user = await this.userService.user({ email: params.email }); //usa o metodo user de UserService para localizar um user por meio do email
     if (!user) throw new NotFoundException('User not found');
-    const passwordMatch = await bcrypt.compare(params.password, user.password); //retorna um true ou false
+    const passwordMatch = await bcrypt.compare(params.password, user.password); //compara a senha digitada com o hash no sistema, retorna um true ou false
     if (!passwordMatch) throw new UnauthorizedException('Invalide credentials');
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { password, ...result } = user;
-
-    return result; //retorna tudo menos a password
+    const payload = { sub: user.id }; //esse sub é padrão do jwt
+    //retorna o acssestoken em formato de objeto passando o id (dento de payload)
+    return {access_token: await this.jwtService.signAsync(payload)}; 
   }
 }
